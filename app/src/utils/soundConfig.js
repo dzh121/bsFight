5782a8316a2410106be8f61e077dea80d3c71869
Dzh1212 <daniel@tondo-iot.com>
2026-03-11 13:00:33 +0200

    Enable sounds and adjust volume settings

--- app/src/utils/soundConfig.js
+++ app/src/utils/soundConfig.js
@@ -13,33 +13,33 @@
  */
 
 const soundConfig = {
-  enabled: false,
-  masterVolume: 0.6,
+  enabled: true,
+  masterVolume: 1,
 
   events: {
     // Battle flow
-    battleStart:   { src: "/sounds/Dmmm.mp3", volume: 0.8 },
-    countdown:     { src: "/sounds/Tick Tock.mp3", volume: 0.7 },
-    turnStart:     { src: "/sounds/whopuuuuf.mp3", volume: 0.3 },
+    battleStart: { src: "/sounds/Dmmm.mp3", volume: 0.8 },
+    countdown: { src: "/sounds/Tick Tock.mp3", volume: 0.7 },
+    turnStart: { src: "/sounds/whopuuuuf.mp3", volume: 0.3 },
 
     // Attacks
-    normalHit:     { src: "/sounds/punch.mp3" },
-    criticalHit:   { src: "/sounds/BOOMMMM.mp3", volume: 0.9 },
-    specialMove:   { src: "/sounds/Grrrtr.mp3", volume: 0.9 },
-    combo:         { src: "/sounds/Wuhddsh.mp3", volume: 0.8 },
+    normalHit: { src: "/sounds/punch.mp3" },
+    criticalHit: { src: "/sounds/BOOMMMM.mp3", volume: 0.9 },
+    specialMove: { src: "/sounds/Grrrtr.mp3", volume: 0.9 },
+    combo: { src: "/sounds/Wuhddsh.mp3", volume: 0.8 },
 
     // Defense
-    dodge:         { src: "/sounds/WHoosh.mp3" },
-    counter:       { src: "/sounds/SWSFF.mp3", volume: 0.7 },
+    dodge: { src: "/sounds/WHoosh.mp3" },
+    counter: { src: "/sounds/SWSFF.mp3", volume: 0.7 },
 
     // Status
-    heal:          { src: "/sounds/wiIIP.mp3" },
-    poison:        { src: "/sounds/WuWuWuWu.mp3" },
-    buff:          { src: "/sounds/wiIIP.mp3" },
+    heal: { src: "/sounds/wiIIP.mp3" },
+    poison: { src: "/sounds/WuWuWuWu.mp3" },
+    buff: { src: "/sounds/wiIIP.mp3" },
 
     // Outcome
-    ko:            { src: "/sounds/punch wuch.mp3", volume: 1.0 },
-    victory:       { src: "/sounds/Tinnn.mp3", volume: 0.9 },
+    ko: { src: "/sounds/punch wuch.mp3", volume: 1.0 },
+    victory: { src: "/sounds/Tinnn.mp3", volume: 0.9 },
     championCrown: { src: "/sounds/TOONTunTunTOON.mp3", volume: 1.0 },
   },
 };
